d728c5c633b5ddd045612d70da4b10693a5cae62
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-22 15:41:05 +0300

    install mongoose and db connection setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+const initDb = require('./config/db.config');
+const { DB_CONNECTION_URI } = require('./config/statics.config');
 const router = require('./router');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, './public')));
 require('./config/handlebars.config')(app);
 app.use(router);
-
-app.listen(
-  5000,
-  console.log.bind(console, 'Server is working on http://localhost:5000')
-);
+initDb(DB_CONNECTION_URI)
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .then(() => {
+    app.listen(
+      5000,
+      console.log.bind(console, 'Server is working on http://localhost:5000')
+    );
+  });
---
+++ src/config/statics.config.js
@@ -0,0 +1,1 @@
+exports.DB_CONNECTION_URI = 'mongodb://localhost:27017/bookings';
